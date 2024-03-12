dc8e30f22c9f6edb3ca33bd5661453e1c5ce55d8
nice-switch <kevin@senpai.zone>
2024-03-12 15:03:08 -0400

    WOOOOH! Making progress!

--- src/main.py
+++ src/main.py
@@ -10,8 +10,14 @@ odv.database.model.change_database_to(
     create_tables=True
 )
 
-account = odv.database.create_account(
-    "test_username",
-    "test_password"
-)
+print(odv.database.generic_peewee_get(
+    odv.database.model.Account,
+    odv.database.model.Account.username,
+    "test_username"
+))
 
+#account = odv.database.create_account(
+#    "test_username",
+#    "test_password"
+#)
+#print(account.account_model.username)
--- src/odv/database/__init__.py
+++ src/odv/database/__init__.py
@@ -2,12 +2,50 @@ from odv import enum
 from odv.database import model, wrapper
 
 
+def generic_peewee_get(target_model: model.BaseModel, model_property, target_value) -> model.BaseModel | None:
+    result_model: target_model | None = None
+
+    try:
+        result_model = target_model.get(
+            model_property == target_value
+        )
+    
+    except model.peewee.DoesNotExist:
+        pass
+    
+    return result_model
+
+
 def get_account_by_username(username: str) -> wrapper.AccountWrapper | None:
-    pass
+    account_wrapper: wrapper.AccountWrapper | None = None
+
+    try:
+        account_wrapper = wrapper.AccountWrapper(
+            model.Account.get(
+                model.Account.username == username
+            )
+        )
+    
+    except model.peewee.DoesNotExist:
+        pass
+
+    return account_wrapper
 
 
 def get_account_by_email(email: str) -> wrapper.AccountWrapper | None:
-    pass
+    account_wrapper: wrapper.AccountWrapper | None = None
+
+    try:
+        account_wrapper = wrapper.AccountWrapper(
+            model.Account.get(
+                model.Account.email == email
+            )
+        )
+    
+    except model.peewee.DoesNotExist:
+        pass
+
+    return account_wrapper
 
 
 def create_account(username: str, password: str, email: str | None = None) -> wrapper.AccountWrapper | None:
@@ -17,3 +55,13 @@ def create_account(username: str, password: str, email: str | None = None) -> wr
     assert get_account_by_username(username) is None, "An account with this username already exists!"
     assert email is None or get_account_by_email(email) is None, "An account with this email already exists!"
     
+    account_wrapper = wrapper.AccountWrapper(
+        model.Account.create(
+            username=username,
+            email=email or ""
+        )
+    )
+
+    account_wrapper.account_model.save()
+
+    return account_wrapper
